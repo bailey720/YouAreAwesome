384ec08ef5dcb5edc23a65c662c1b144bc8b4c6f
Brian P. Bailey <bailey720@gmail.com>
2026-05-05 16:42:04 -0600

    after chapter 1.8

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var imageName = ""
     @State private var message = ""
+    @State private var imageNumber = 0
     
     var body: some View {
         
@@ -29,14 +30,18 @@ struct ContentView: View {
             
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-                let imageName1 = "image0"
-                let imageName2 = "image1"
                 
                 message = message == message1 ? message2 : message1
-                imageName = imageName == imageName1 ? imageName2 : imageName1
+
+                imageName = "image\(imageNumber)"
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
